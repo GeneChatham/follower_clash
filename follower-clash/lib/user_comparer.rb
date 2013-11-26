25dd5971a0cb540a8e476d13eb8191db5406805d
GeneChatham <genechatham@gmail.com>
2013-11-26 11:36:26 -0600

    Added initialization method in user_comparer

---
+++ follower-clash/lib/user_comparer.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'twitter'
+
+module UserComparer
+	class User
+
+	end
+
+	class Comparer
+		def initialize(user1, user2)
+		end
+
+		def compare
+		end
+	end
+end
